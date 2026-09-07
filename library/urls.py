0da4e26232310059b139a619c51e010c647eb442
Льоня Крайнічук <lonakrajnicuk@gmail.com>
2026-09-08 02:05:54 +0300

    HW16: refactor views to Generic Views (ListCreateAPIView, RetrieveUpdateDestroyAPIView), add AvailableBookListAPIView

--- library/urls.py
+++ library/urls.py
@@ -5,10 +5,15 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
-    path('authors/', views.author_list, name='author_list'),
-    path('authors/<int:pk>/', views.author_detail, name='author_detail'),
-    path('books/', views.book_list, name='book_list'),
-    path('books/available/', views.available_books, name='available_books'),
-    path('books/<int:pk>/', views.book_detail, name='book_detail'),
-    path('borrowings/', views.borrowing_list, name='borrowing_list'),
+    # Authors
+    path('authors/', views.AuthorListCreateAPIView.as_view(), name='author_list_create'),
+    path('authors/<int:author_id>/', views.AuthorDetailAPIView.as_view(), name='author_detail'),
+
+    # Books
+    path('books/', views.BookListCreateAPIView.as_view(), name='book_list_create'),
+    path('books/available/', views.AvailableBookListAPIView.as_view(), name='available_books'),
+    path('books/<int:pk>/', views.BookDetailAPIView.as_view(), name='book_detail'),
+
+    # Borrowings
+    path('borrowings/', views.BorrowingListCreateAPIView.as_view(), name='borrowing_list_create'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Author, Book, Borrowing
 from .serializers import (
@@ -12,43 +9,44 @@ from .serializers import (
 )
 
 
-@api_view(['GET'])
-def author_list(request):
-    authors = Author.objects.all()
-    serializer = AuthorSerializer(authors, many=True)
-    return Response({'count': authors.count(), 'data': serializer.data})
+class AuthorListCreateAPIView(generics.ListCreateAPIView):
+    """GET: list all authors. POST: create a new author."""
+
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
 
-@api_view(['GET'])
-def author_detail(request, pk):
-    author = get_object_or_404(Author, pk=pk)
-    serializer = AuthorSerializer(author)
-    return Response(serializer.data)
+class AuthorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """GET/PUT/PATCH/DELETE a single author, looked up by `author_id`."""
+
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    lookup_url_kwarg = 'author_id'
 
 
-@api_view(['GET'])
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response({'count': books.count(), 'data': serializer.data})
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    """GET: list all books. POST: create a new book."""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
-@api_view(['GET'])
-def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    serializer = BookDetailSerializer(book)
-    return Response(serializer.data)
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """GET/PUT/PATCH/DELETE a single book."""
+
+    queryset = Book.objects.all()
+    serializer_class = BookDetailSerializer
 
 
-@api_view(['GET'])
-def borrowing_list(request):
-    borrowings = Borrowing.objects.all()
-    serializer = BorrowingSerializer(borrowings, many=True)
-    return Response({'count': borrowings.count(), 'data': serializer.data})
+class BorrowingListCreateAPIView(generics.ListCreateAPIView):
+    """GET: list all borrowings. POST: register a new borrowing."""
+
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
 
 
-@api_view(['GET'])
-def available_books(request):
-    books = Book.objects.filter(available_copies__gt=0)
-    serializer = BookSerializer(books, many=True)
-    return Response({'count': books.count(), 'data': serializer.data})
+class AvailableBookListAPIView(generics.ListAPIView):
+    """GET: list only the books that currently have available copies."""
+
+    queryset = Book.objects.filter(available_copies__gt=0)
+    serializer_class = BookSerializer
